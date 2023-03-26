09869f0cef5e63bffd3ff2ecd1154678892b4fd9
Bartlomiej Snochowski <bartlomiej.snochowski@gmail.com>
2023-03-27 00:55:49 +0200

    added board with tiles

---
+++ game.js
@@ -0,0 +1,9 @@
+const gameTiles = document.querySelectorAll('.tile');
+const gameBoard = document.querySelector('#game-board');
+
+const gameState = [
+    [gameTiles[0], gameTiles[1], gameTiles[2]],
+    [gameTiles[3], gameTiles[4], gameTiles[5]],
+    [gameTiles[6], gameTiles[7], gameTiles[8]],
+];
+
